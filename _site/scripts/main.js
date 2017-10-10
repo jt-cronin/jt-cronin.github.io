b0edac7d7a8268078e98b1088a4c8f13e9eaad64
John T Cronin <jt.cronin@outlook.com>
2017-10-09 19:03:46 -0500

    updated resume doc

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
 	var dropDownContent = document.getElementsByClassName('hideShow')[0];
 	var dropDownMenu = document.getElementById('dropDownB');
 	var shown = false;
+	var backButton = document.getElementsByClassName('backButton')[0];
 
 	dropDownMenu.addEventListener('click', function(){
 		if (shown == false){
@@ -15,6 +16,10 @@ window.addEventListener('load', function(){
 
 		}
 		
+	});
+	
+	backButton.addEventListener('click', function(){
+		window.history.back();
 	});
 
 });
